7a113c0293b64a2a90fcaf7f33ad0dbbf2562fef
ScharxiDev <scharxidev@outlook.de>
2022-03-12 12:57:59 +0100

    feature: soft-ban command ✔

--- src/main/kotlin/me/scharxidev/odin/util/ResponseHelper.kt
+++ src/main/kotlin/me/scharxidev/odin/util/ResponseHelper.kt
@@ -1,11 +1,13 @@
 package me.scharxidev.odin.util
 
 import com.kotlindiscord.kord.extensions.DISCORD_BLACK
+import com.kotlindiscord.kord.extensions.utils.dm
 import dev.kord.common.Color
 import dev.kord.core.behavior.channel.MessageChannelBehavior
 import dev.kord.core.behavior.channel.createEmbed
 import dev.kord.core.entity.Message
 import dev.kord.core.entity.User
+import dev.kord.rest.builder.message.create.embed
 import kotlinx.datetime.Clock
 
 object ResponseHelper {
@@ -29,4 +31,21 @@ object ResponseHelper {
             }
         }
     }
+
+    suspend fun userDmEmbed(
+        user: User,
+        embedTitle: String?,
+        embedDesc: String?,
+        embedColor: Color?,
+    ): Message? {
+        return user.dm {
+            embed {
+                embedTitle?.let { title = it }
+                embedDesc?.let { description = it }
+                color = embedColor ?: DISCORD_BLACK
+                timestamp = Clock.System.now()
+            }
+        }
+    }
+
 }
